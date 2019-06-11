4f0d7b39dcc905af619dd2460150ba452da34e03
Meghan Miller <mille265@gmail.com>
2019-06-11 11:50:23 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
+  get 'accounts/new'
+
+  get 'accounts/create'
+
+  get 'accounts/edit'
+
+  get 'accounts/update'
+
+  get 'accounts/destroy'
+
+  get 'accounts/index'
+
+  get 'accounts/show'
+
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/fiancials'
+
+  get 'admin/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'categories/show'
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts
   resources :categories
 end
